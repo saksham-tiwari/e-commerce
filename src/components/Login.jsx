ff4d9739341d53c416cd2520fe59beb8a991bd79
saksham-tiwari <lucky121saty@gmail.com>
2021-10-29 01:05:09 +0530

    added svg, images and started login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import globe from '../assets/globe.svg'
+import delivery from '../assets/delivery.jpg'
+
+const Login = () => {
+    return (
+        <div>
+            <img src = {globe} alt="globe"/>
+            <img src = {delivery} alt="delivery"/>
+        </div>
+    )
+}
+
+export default Login
