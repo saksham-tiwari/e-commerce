a82b47721cfd81e510c89c537857df0096163109
saksham-tiwari <lucky121saty@gmail.com>
2021-11-09 22:37:44 +0530

    Fixed responsiveness of navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Container, Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap'
+import { Container, Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap'
+import bag from "../assets/shoppingbag.svg"
 
 const NavBar = () => {
     return (
@@ -21,14 +22,26 @@ const NavBar = () => {
     <Navbar.Collapse id="navbarScroll">
       <Nav
         className="me-auto my-2 my-lg-0 navlinks"
-        style={{ maxHeight: '100px' }}
+        style={{ maxHeight: '400px' }}
         navbarScroll
       >
-        <Nav.Link href="#action1">Home</Nav.Link>
+      
+        <Nav.Link href="#action1" className="homelink">Home</Nav.Link>
         <Nav.Link href="#action2">Shop</Nav.Link>
         <Nav.Link href="#action2">Wishlist</Nav.Link>
         <Nav.Link href="#action2">Help</Nav.Link>
-        <Button variant="primary">Log In</Button>        
+        <Button variant="primary" className="login-button">Log In</Button>  
+        <Nav.Link href="#action2"><img src={bag} alt="bag"/></Nav.Link>
+        <Form className="scrollsearch">
+          <FormControl
+            type="search"
+            placeholder="Search"
+            className="me-2"
+            aria-label="Search"
+          />
+          <Button variant="outline-success">Search</Button>
+        </Form>
+
       </Nav>
       
     </Navbar.Collapse>
