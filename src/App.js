c3ebd03096e42875c1b1f67b3839ef88bb09c1b3
Saksham Tiwari <sakshamt234@gmail.com>
2021-11-12 21:33:56 +0530

    Created checkout page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ChangePassword from "./components/auth/ChangePassword";
 import Forgot from "./components/auth/Forgot";
 import Login from "./components/auth/Login";
@@ -14,9 +10,7 @@ import Wishlist from "./components/layout/Wishlist/Wishlist";
 import Footer from "./components/layout/Footer";
 import Cart from "./components/layout/Cart/Cart";
 import ProductsPage from "./components/layout/ProductsPage/ProductsPage";
-
-
-
+import Checkout from "./components/layout/Checkout/Checkout";
 
 function App() {
   return (
@@ -24,42 +18,46 @@ function App() {
       {/* <NavBar/> */}
 
       <Switch>
-      <Route exact path="/">
-          <NavBar/>
-          <Home/>
-          <Footer/>
-
-      </Route>
-      <Route exact path="/login">
-          <Login/>
-      </Route>
-      <Route exact path="/forgot-password">
-          <Forgot/>
-      </Route>
-      <Route exact path="/signup">
-          <SignUp/>
-      </Route>
-      <Route exact path="/change-password">
-          <ChangePassword/>
-      </Route>
-      <Route exact path="/otp">
-          <Otp/>
-      </Route>
-      <Route exact path="/wishlist">
-          <NavBar/>
-          <Wishlist/>
-          <Footer/>
-      </Route>
-      <Route exact path="/cart">
-          <NavBar/>
-          <Cart/>
-          <Footer/>
-      </Route>
-      <Route exact path="/products-page">
-          <NavBar/>
-          <ProductsPage/>
-          <Footer/>
-      </Route>
+        <Route exact path="/">
+          <NavBar />
+          <Home />
+          <Footer />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/forgot-password">
+          <Forgot />
+        </Route>
+        <Route exact path="/signup">
+          <SignUp />
+        </Route>
+        <Route exact path="/change-password">
+          <ChangePassword />
+        </Route>
+        <Route exact path="/otp">
+          <Otp />
+        </Route>
+        <Route exact path="/wishlist">
+          <NavBar />
+          <Wishlist />
+          <Footer />
+        </Route>
+        <Route exact path="/cart">
+          <NavBar />
+          <Cart />
+          <Footer />
+        </Route>
+        <Route exact path="/products-page">
+          <NavBar />
+          <ProductsPage />
+          <Footer />
+        </Route>
+        <Route exact path="/checkout">
+          <NavBar />
+          <Checkout />
+          <Footer />
+        </Route>
       </Switch>
     </Router>
   );
---
+++ src/components/layout/Checkout/Checkout.jsx
@@ -0,0 +1,96 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+const Cart = () => {
+  return (
+    <div>
+      <h1 className="whole">Checkout</h1>
+      <hr className="hr-wish" />
+      <Container>
+        <Row>
+          <Col>Item</Col>
+          <Col xs lg="2">
+            Qty
+          </Col>
+          <Col xs lg="2">
+            Price
+          </Col>
+        </Row>
+        <Row>
+          <Col>Image and Product Desc</Col>
+          <Col xs lg="2">
+            1
+          </Col>
+          <Col xs lg="2">
+            $300.00
+          </Col>
+        </Row>
+        <Row>
+          <Col>Image and Product Desc</Col>
+          <Col xs lg="2">
+            1
+          </Col>
+          <Col xs lg="2">
+            $300.00
+          </Col>
+        </Row>
+        <Row>
+          <Col>Image and Product Desc</Col>
+          <Col xs lg="2">
+            1
+          </Col>
+          <Col xs lg="2">
+            $300.00
+          </Col>
+        </Row>
+        <Row>
+          <Col>Image and Product Desc</Col>
+          <Col xs lg="2">
+            1
+          </Col>
+          <Col xs lg="2">
+            $300.00
+          </Col>
+        </Row>
+      </Container>
+      <div style={{ width: "50%", marginLeft: "25%" }}>
+        <Container>
+          <Row>
+            <Col>Subtotal</Col>
+            <Col>$1,200.00</Col>
+          </Row>
+          <Row>
+            <Col>Shipping Fee</Col>
+            <Col>$0.00</Col>
+          </Row>
+          <Row>
+            <Col>Promo Code</Col>
+            <Col>-$0.00</Col>
+          </Row>
+          <Row>
+            <Col>Tax</Col>
+            <Col>$0.00</Col>
+          </Row>
+          <hr />
+          <Row>
+            <Col>Total</Col>
+            <Col>$1,200.00</Col>
+          </Row>
+        </Container>
+        <br />
+        <br />
+        <Container>
+          <Row>
+            <Col>Deliver to:</Col>
+            <Col>
+              XYZ
+              <br />
+              Address
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
